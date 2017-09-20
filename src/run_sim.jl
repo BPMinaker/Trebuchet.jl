d069a5a59a994cca7bc6975279229b622466d227
BPMinaker <bminaker@uwindsor.ca>
2017-09-20 14:04:10 -0400

    Time shortened to 4.0

--- src/run_sim.jl
+++ src/run_sim.jl
@@ -24,7 +24,7 @@ println(params)
 
 dummy(t,xx)=eqn_of_motion(t,xx,params)
 
-tout=(0.0,7.0)
+tout=(0.0,4.0)
 x0=[params.theta0*(pi/180), 0.0, 0.0, params.l5-cos(params.theta0*(pi/180))*params.l4, -params.l4*sin(params.theta0*(pi/180)), 0.0, 0.0, 0.0, 0.0, 0.0]
 prob=ODEProblem(dummy,x0,tout)
 sol=solve(prob,DP5(),saveat=0.0005)
