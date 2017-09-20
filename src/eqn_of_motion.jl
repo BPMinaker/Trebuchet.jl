47176e21b69fa343605651ff88ebb18bdde85760
BPMinaker <bminaker@uwindsor.ca>
2017-09-20 17:52:22 -0400

    Error fix

--- src/eqn_of_motion.jl
+++ src/eqn_of_motion.jl
@@ -27,22 +27,22 @@ ReleaseAngle=params.ReleaseAngle #parameter name changed
 # MaxHeight=params.MaxHeight #parameter name changed
 # Distance=params.Distance #parameter name changed
 
-M=[params.m1*params.l2^2+params.m2*params.l1^2+params.I -sin(theta1-theta2)*params.m1*params.l2*params.l3 -(params.m1+params.m2)*params.l2*sin(theta1) 0 0
-    -sin(theta1-theta2)*params.m1*params.l2*params.l3 params.m1*params.l3^2 params.m1*params.l3*cos(theta2) 0 0
-    -(params.m1*params.l2+params.m2*params.l1)*sin(theta1) params.m1*params.l3*cos(theta2) params.m1+params.m2+params.m3 0 0
-    0 0 0 params.m4 0
-    0 0 0 0 params.m4]
+M=[m1*l2^2+m2*l1^2+I -sin(theta1-theta2)*m1*l2*l3 -(m1*l2+m2*l1)*sin(theta1) 0 0;
+-sin(theta1-theta2)*m1*l2*l3 m1*l3^2 m1*l3*cos(theta2) 0 0;
+-(m1*l2+m2*l1)*sin(theta1) m1*l3*cos(theta2) m1+m2+m3 0 0;
+0 0 0 m4 0;
+0 0 0 0 m4]
 
-	B=[2*params.l4*u*sin(theta1)-2*params.l4*x*sin(theta1)+2*params.l4*y*cos(theta1) 0 2*u-2*x-2*params.l4*cos(theta1) 2*x-2*u+2*params.l4*cos(theta1) 2*y+2*params.l4*sin(theta1)
-	    0 0 0 0 1]
+B=[2*l4*u*sin(theta1)-2*l4*x*sin(theta1)+2*l4*y*cos(theta1) 0 2*u-2*x-2*l4*cos(theta1) 2*x-2*u+2*l4*cos(theta1) 2*y+2*l4*sin(theta1);
+0 0 0 0 1]
 
-	f=[-cos(theta1-theta2)*params.m1*params.l2*params.l3*dtheta2^2-params.m1*params.g*params.l2*cos(theta1)-params.m2*params.g*params.l1*cos(theta1)
-	    cos(theta1-theta2)*params.m1*params.l2*params.l3*dtheta1^2-params.m1*params.g*params.l3*sin(theta2)
-	    (params.m1*params.l2+params.m2*params.l1)*cos(theta1)*dtheta1^2+params.m1*params.l3*sin(theta2)*dtheta2^2
+	f=[-cos(theta1-theta2)*m1*l2*l3*dtheta2^2-m1*g*l2*cos(theta1)-m2*g*l1*cos(theta1)
+	    cos(theta1-theta2)*m1*l2*l3*dtheta1^2-m1*g*l3*sin(theta2)
+	    (m1*l2+m2*l1)*cos(theta1)*dtheta1^2+m1*l3*sin(theta2)*dtheta2^2
 	    0
-	    -params.m4*params.g]
+	    -m4*g]
 
-	NdBdq=[-(2*params.l4*sin(theta1)*du+2*params.l4*u*cos(theta1)*dtheta1-2*params.l4*sin(theta1)*dx-2*params.l4*x*cos(theta1)*dtheta1+2*params.l4*cos(theta1)*dy-2*params.l4*y*sin(theta1)*dtheta1)*dtheta1-(2*du-2*dx+2*params.l4*sin(theta1)*dtheta1)*du-(2*dx-2*du-2*params.l4*sin(theta1)*dtheta1)*dx-(2*dy+2*params.l4*cos(theta1)*dtheta1)*dy
+	NdBdq=[-(2*l4*sin(theta1)*du+2*l4*u*cos(theta1)*dtheta1-2*l4*sin(theta1)*dx-2*l4*x*cos(theta1)*dtheta1+2*l4*cos(theta1)*dy-2*l4*y*sin(theta1)*dtheta1)*dtheta1-(2*du-2*dx+2*l4*sin(theta1)*dtheta1)*du-(2*dx-2*du-2*l4*sin(theta1)*dtheta1)*dx-(2*dy+2*l4*cos(theta1)*dtheta1)*dy
 	0]
 
 	lmd=(B*(M\B'))\((B*(M\f))-NdBdq)
@@ -56,10 +56,10 @@ M=[params.m1*params.l2^2+params.m2*params.l1^2+params.I -sin(theta1-theta2)*para
 	      println("Lift Off Time= ",t)
 	    end
 	    params.flag1=true
-	    if ((ra<=params.ReleaseAngle*(pi/180) && ra>0.00001) || params.flag2)
+	    if ((ra<=ReleaseAngle*(pi/180) && ra>0.00001) || params.flag2)
 	        if (~params.flag2)
 	            actual_ReleaseAngle=ra*(180/pi)
-	            string_angle=asin((y+params.l4*sin(theta1))/params.l5)*(180/pi)
+	            string_angle=asin((y+l4*sin(theta1))/l5)*(180/pi)
 	            Release_X_velocity=dx
 	            Release_Y_velocity=dy
 	            Release_combined_velocity=sqrt(dx^2+dy^2)
@@ -84,4 +84,3 @@ M=[params.m1*params.l2^2+params.m2*params.l1^2+params.I -sin(theta1-theta2)*para
 
 	xxdot
 	end
-	
