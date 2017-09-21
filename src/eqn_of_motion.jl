18cb95d2540a1034e3ba9af006e9d35f6e5177b7
BPMinaker <bminaker@uwindsor.ca>
2017-09-21 17:44:15 -0400

    Optimization

--- src/eqn_of_motion.jl
+++ src/eqn_of_motion.jl
@@ -27,60 +27,69 @@ ReleaseAngle=params.ReleaseAngle #parameter name changed
 # MaxHeight=params.MaxHeight #parameter name changed
 # Distance=params.Distance #parameter name changed
 
-M=[m1*l2^2+m2*l1^2+I -sin(theta1-theta2)*m1*l2*l3 -(m1*l2+m2*l1)*sin(theta1) 0 0;
--sin(theta1-theta2)*m1*l2*l3 m1*l3^2 m1*l3*cos(theta2) 0 0;
--(m1*l2+m2*l1)*sin(theta1) m1*l3*cos(theta2) m1+m2+m3 0 0;
+st1=sin(theta1)
+ct1=cos(theta1)
+st21=sin(theta2-theta1)
+ct12=cos(theta1-theta2)
+st2=sin(theta2)
+ct2=cos(theta2)
+
+
+M=[m1*l2^2+m2*l1^2+I st21*m1*l2*l3 -(m1*l2+m2*l1)*st1 0 0;
+st21*m1*l2*l3 m1*l3^2 m1*l3*ct2 0 0;
+-(m1*l2+m2*l1)*st1 m1*l3*ct2 m1+m2+m3 0 0;
 0 0 0 m4 0;
 0 0 0 0 m4]
 
-B=[2*l4*u*sin(theta1)-2*l4*x*sin(theta1)+2*l4*y*cos(theta1) 0 2*u-2*x-2*l4*cos(theta1) 2*x-2*u+2*l4*cos(theta1) 2*y+2*l4*sin(theta1);
+B=[2*l4*u*st1-2*l4*x*st1+2*l4*y*ct1 0 2*u-2*x-2*l4*ct1 2*x-2*u+2*l4*ct1 2*y+2*l4*st1;
 0 0 0 0 1]
 
-	f=[-cos(theta1-theta2)*m1*l2*l3*dtheta2^2-m1*g*l2*cos(theta1)-m2*g*l1*cos(theta1)
-	    cos(theta1-theta2)*m1*l2*l3*dtheta1^2-m1*g*l3*sin(theta2)
-	    (m1*l2+m2*l1)*cos(theta1)*dtheta1^2+m1*l3*sin(theta2)*dtheta2^2
-	    0
-	    -m4*g]
+f=[-ct12*m1*l2*l3*dtheta2^2-m1*g*l2*ct1-m2*g*l1*ct1
+ct12*m1*l2*l3*dtheta1^2-m1*g*l3*st2
+(m1*l2+m2*l1)*ct1*dtheta1^2+m1*l3*st2*dtheta2^2
+0
+-m4*g]
 
-	NdBdq=[-(2*l4*sin(theta1)*du+2*l4*u*cos(theta1)*dtheta1-2*l4*sin(theta1)*dx-2*l4*x*cos(theta1)*dtheta1+2*l4*cos(theta1)*dy-2*l4*y*sin(theta1)*dtheta1)*dtheta1-(2*du-2*dx+2*l4*sin(theta1)*dtheta1)*du-(2*dx-2*du-2*l4*sin(theta1)*dtheta1)*dx-(2*dy+2*l4*cos(theta1)*dtheta1)*dy
-	0]
+NdBdq=[-(2*l4*st1*du+2*l4*u*ct1*dtheta1-2*l4*st1*dx-2*l4*x*ct1*dtheta1+2*l4*ct1*dy-2*l4*y*st1*dtheta1)*dtheta1-(2*du-2*dx+2*l4*st1*dtheta1)*du-(2*dx-2*du-2*l4*st1*dtheta1)*dx-(2*dy+2*l4*ct1*dtheta1)*dy
+0]
 
-	lmd=(B*(M\B'))\((B*(M\f))-NdBdq)
-	d2q=M\(-B'*lmd+f)
+lmd=(B*(M\B'))\((B*(M\f))-NdBdq)
+d2q=M\(-B'*lmd+f)
 
-	ra=atan2(dy,dx)
-	if (lmd[2]>0 || params.flag1)
-	    lmd=(B[1,:]'*(M\B[1,:]))\((B[1,:]'*(M\f))+(-NdBdq[1,:]'))
-	    d2q=M\((-B[1,:]*lmd)+f)
-	    if(~params.flag1)
-	      println("Lift Off Time= ",t)
-	    end
-	    params.flag1=true
-	    if ((ra<=ReleaseAngle*(pi/180) && ra>0.00001) || params.flag2)
-	        if (~params.flag2)
-	            actual_ReleaseAngle=ra*(180/pi)
-	            string_angle=asin((y+l4*sin(theta1))/l5)*(180/pi)
-	            Release_X_velocity=dx
-	            Release_Y_velocity=dy
-	            Release_combined_velocity=sqrt(dx^2+dy^2)
-	            Release_t=t
-	            Release_x=x
-	            Release_y=y
-	            println("Actual Release Angle= ", actual_ReleaseAngle)
-	            println("String Angle= ",string_angle)
-	            println("Release X velocity= ",Release_X_velocity)
-	            println("Release Y velocity= ",Release_Y_velocity)
-	            println("Release Combined velocity= ", Release_combined_velocity)
-	            println("Release time= ",Release_t)
-	            println("Release x= ",Release_x)
-	            println("Release y= ",Release_y)
-	        end
-	        d2q=M\f
-	        params.flag2=true
-	    end
+ra=atan2(dy,dx)
+if (lmd[2]>0 || params.flag1)
+	lmd=(B[1,:]'*(M\B[1,:]))\((B[1,:]'*(M\f))+(-NdBdq[1,:]'))
+	d2q=M\((-B[1,:]*lmd)+f)
+	if(~params.flag1)
+		println("Lift Off Time= ",t)
 	end
-
-	xxdot=[dtheta1, dtheta2, du, dx, dy, d2q[1], d2q[2], d2q[3], d2q[4], d2q[5]]
-
-	xxdot
+	params.flag1=true
+	if ((ra<=ReleaseAngle*(pi/180) && ra>0.00001) || params.flag2)
+		if (~params.flag2)
+			actual_ReleaseAngle=ra*(180/pi)
+			string_angle=asin((y+l4*st1)/l5)*(180/pi)
+			Release_X_velocity=dx
+			Release_Y_velocity=dy
+			Release_combined_velocity=sqrt(dx^2+dy^2)
+			Release_t=t
+			Release_x=x
+			Release_y=y
+			println("Actual Release Angle= ", actual_ReleaseAngle)
+			println("String Angle= ",string_angle)
+			println("Release X velocity= ",Release_X_velocity)
+			println("Release Y velocity= ",Release_Y_velocity)
+			println("Release Combined velocity= ", Release_combined_velocity)
+			println("Release time= ",Release_t)
+			println("Release x= ",Release_x)
+			println("Release y= ",Release_y)
+		end
+		d2q=M\f
+		params.flag2=true
 	end
+end
+
+xxdot=[dtheta1, dtheta2, du, dx, dy, d2q[1], d2q[2], d2q[3], d2q[4], d2q[5]]
+
+xxdot
+
+end
